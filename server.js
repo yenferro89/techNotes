8fdbcff20eef1ad70d6bed2d2c10a5ca5449d106
Yen Pinero Ferro <ypine022@fiu.edu>
2022-10-18 22:51:00 -0400

    commit on "server"

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3500
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3500;
 
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
