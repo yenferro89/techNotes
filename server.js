f937084aa3b794d975480ff217b64213407380df
Yen Pinero Ferro <ypine022@fiu.edu>
2022-10-21 16:53:16 -0400

    commit on "server, corsOptions, errorHandler"

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const errorHandler = require("./middleware/errorHandler");
 const { logger } = require("./middleware/logger");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions")
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
 
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use(express.json());
 
