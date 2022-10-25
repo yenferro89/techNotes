1ccb4d24620cde38a39bf8bfe68ff1aae12041bc
Yen Pinero Ferro <ypine022@fiu.edu>
2022-10-25 18:31:07 -0400

    commit on "userController and User"

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,7 +8,7 @@ const bcrypt = require("bcrypt");
 //@access Private
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select("-password").lean();
-  if (!users) {
+  if (!users?.length) {
     return res.status(400).json({ message: "No user found" });
   }
   return res.json(users);
@@ -26,8 +26,8 @@ const createNewUser = asyncHandler(async (req, res) => {
   }
 
   //Check for duplicates
-  const duplicates = await User.findOne({ username }).lean().exec();
-  if (duplicates) {
+  const duplicate = await User.findOne({ username }).lean().exec();
+  if (duplicate) {
     return res.status(409).json({ message: "Duplicate username" });
   }
 
@@ -47,12 +47,69 @@ const createNewUser = asyncHandler(async (req, res) => {
 //@desc Update user
 //@route PATCH /users
 //@access Private
-const updateUser = asyncHandler(async (req, res) => {});
+const updateUser = asyncHandler(async (req, res) => {
+  const { id, username, password, roles, active } = req.body;
+
+  if (
+    !id ||
+    !username ||
+    !Array.isArray(roles) ||
+    !roles.length ||
+    typeof active !== "boolean"
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const user = await User.findById(id).exec();
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  //Check duplicate
+  const duplicate = await User.findOne({ username }).lean().exec();
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: "Duplicate username" });
+  }
+
+  //Update user
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
+
+  if (password) {
+    user.password = await bcrypt.hash(password, 10); //salt rounds
+  }
+
+  const updateUser = await user.save();
+
+  res.json({ message: `${updateUser.username} updated` });
+});
 
 //@desc Delete user
 //@route DELETE /users
 //@access Private
-const deleteUser = asyncHandler(async (req, res) => {});
+const deleteUser = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+
+  if (!id) {
+    res.status(400).json({ message: "User ID required" });
+  }
+
+  //Check if the user has notes assigned
+  const note = await Note.findOne({ user: id }).lean().exec();
+  if (note) {
+    return res.status(400).json({ message: "User has assigned notes" });
+  }
+
+  const user = await User.findById(id).exec();
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  const result = await user.deleteOne();
+
+  res.json(`Username ${result.username} with ID ${result._id} deleted`);
+});
 
 module.exports = {
   getAllUsers,
